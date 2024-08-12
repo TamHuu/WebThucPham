db8bb75c147be73a69845d6d13bb2784b1034e44
Huu Tam <Christopherle0902@gmail.com>
2024-08-12 16:36:57 +0700

    form

---
+++ Areas/Admin/Controllers/SanPhamController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+using WebThucPham.Models;
+
+namespace WebThucPham.Areas.Admin.Controllers
+{
+    public class SanPhamController:Controller
+    {
+        public ActionResult DanhSachSanPham(int? namsx, double? donGiaLonHon, double? donGiaNhoHon, string tenSanPham)
+        {
+            ViewBag.namsx = namsx;
+            var danhSach = new SanPham().GetDanhSach();
+
+            return View(danhSach);
+        }
+    }
+}
--- Models/SanPham.cs
+++ Models/SanPham.cs
@@ -2,12 +2,41 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.UI.WebControls;
 
 namespace WebThucPham.Models
 {
     public class SanPham
     {
-        public int ID { get; set; }  
+        public int ID { get; set; }
         public string TenSanPham { get; set; }
+
+        public int NamSanXuat { get; set; }
+
+        public double DonGia { get; set; }
+
+        public string MoTa { get; set; }
+
+        public List<SanPham> GetDanhSach()
+        {
+            List<SanPham> danhSach = new List<SanPham>();
+            danhSach.Add(new SanPham()
+            {
+                ID = 1,
+                TenSanPham = " Fish Basa",
+                NamSanXuat = 2024,
+                DonGia = 1200000,
+                MoTa = "Sell by Kg",
+            });
+            danhSach.Add(new SanPham()
+            {
+                ID = 2,
+                TenSanPham = " Cow meal",
+                NamSanXuat = 2022,
+                DonGia = 2200000,
+                MoTa = "Sell by Kg",
+            });
+            return danhSach;
+        }
     }
 }
