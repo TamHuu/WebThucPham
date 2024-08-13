534b354ba96104be54e2c2bb49746138df14a6d7
Huu Tam <Christopherle0902@gmail.com>
2024-08-13 13:18:32 +0700

    xoá dữ liệu cập nhật dữ liệu

--- Areas/Admin/Controllers/SanPhamController.cs
+++ Areas/Admin/Controllers/SanPhamController.cs
@@ -34,6 +34,7 @@ namespace WebThucPham.Areas.Admin.Controllers
             var kq2 = db.spSanPham(namsx, donGiaLonHon, donGiaNhoHon, tenSanPham).ToList();
             return View(kqTimKiem);
         }
+        #region Thêm mới:
         public ActionResult ThemMoi()
         {
             SanPham spMoi = new SanPham();
@@ -48,7 +49,7 @@ namespace WebThucPham.Areas.Admin.Controllers
         public ActionResult ThemMoi(SanPham spMoi)
         {
             // Bước 1: Check dữ liệu bắt buộc
-            if (string.IsNullOrEmpty(spMoi.TenSanPham) && string.IsNullOrEmpty(spMoi.MoTa) == true) 
+            if (string.IsNullOrEmpty(spMoi.TenSanPham) && string.IsNullOrEmpty(spMoi.MoTa) == true)
             {
                 ModelState.AddModelError("TenSanPham", "Thiếu tên sản phẩm");
                 ModelState.AddModelError("MoTa", "Thiếu mô tả");
@@ -74,7 +75,68 @@ namespace WebThucPham.Areas.Admin.Controllers
             // 2. Return về link cụ thể
             // 3. Link theo router
         }
+
+        #endregion
+
+        #region Cập nhật
+        //2. Tạo form cập nhật
+        public ActionResult CapNhat(string id)
+        {
+            try
+            {
+                var coverId = int.Parse(id);
+                //2.1 Tìm đối tượng muốn cập nhật
+                KH2024_WebBanHangEntities db = new KH2024_WebBanHangEntities();
+                var sanPham = db.SanPhams.Find(coverId);
+                return View(sanPham);
+            }
+            catch
+            {
+                return View(new SanPham());
+            }
+
+            //2.2 Gửi sang model view
+
+
+        }
+        //3. Hàm lưu cập nhật
+        [HttpPost]
+        public ActionResult CapNhat(SanPham model)
+        {
+            //3.1 Tìm đối tượng cần cập nhật
+            KH2024_WebBanHangEntities db = new KH2024_WebBanHangEntities();
+            var sanPham = db.SanPhams.Find(model.ID);
+            //3.2 Gán gái trị nhận được từ model sang đối tượng cần cập nhật
+            sanPham.DonGia = model.DonGia;
+            sanPham.HetHang = model.HetHang;
+            sanPham.MauSac = model.MauSac;
+            sanPham.MoTa = model.MoTa;
+            sanPham.NamSanXuat = model.NamSanXuat;
+            sanPham.PhanLoai = model.PhanLoai;
+            sanPham.TenSanPham = model.TenSanPham;
+            sanPham.SoDienThoaiNCC = model.SoDienThoaiNCC;
+            sanPham.EmailNCC = model.EmailNCC;
+            //3.3. Lưu lại thay đổi
+            db.SaveChanges();
+            //3.4 Chuyển hướng về danh sách
+            return RedirectToAction("DanhSachSanPham");
+        }
+        #endregion
+        public ActionResult Xoa(int id)
+        {
+            //1. Tìm đối tượng cần xoá
+            KH2024_WebBanHangEntities db = new KH2024_WebBanHangEntities();
+            var sanPham = db.SanPhams.Find(id);
+            //2.Xoá 
+            db.SanPhams.Remove(sanPham);  
+            //3. Cập nhật sự thay đổi
+            db.SaveChanges();
+            //4.Chuyển sang trang danh sách sản phẩm
+            return RedirectToAction("DanhSachSanPham");
+        }
     }
 
+
+  
 
 }
