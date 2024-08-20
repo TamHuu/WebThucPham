36bcf59b6540cb0757cc9d83c99d340019e9abe3
Huu Tam <Christopherle0902@gmail.com>
2024-08-20 13:13:42 +0700

    SEO Web

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -6,6 +6,13 @@ public class RouteConfig
     public static void RegisterRoutes(RouteCollection routes)
     {
         routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+        // How It Work
+        routes.MapRoute(
+          name: "Howitwork",
+          url: "how-it-work", // link seo
+          defaults: new { controller = "Home", action = "HowItWork", id = UrlParameter.Optional },
+          namespaces: new string[] { "WebThucPham.Controllers" } // Specify the main project's namespace
+      );
 
         routes.MapRoute(
             name: "Default",
